e442f3f8a09deb1133f97e2d72d2f84c5405b7b6
ash66 <aichiko66@163.com>
2023-04-19 10:59:27 +0800

    就这样吧

--- modules/ssi_service/lib/src/request/exceptions.dart
+++ modules/ssi_service/lib/src/request/exceptions.dart
@@ -1,40 +1,40 @@
 import 'package:dio/dio.dart';
-import 'package:flutter/cupertino.dart';
 
 /// 自定义异常
-class AppException implements Exception {
-   final String message;
-   final int code;
+class HttpException implements Exception {
+  String? _message;
 
-  AppException({required this.code, required this.message});
+  String get message => _message ?? "未知错误";
+
+  int? _code;
+
+  int get code => _code ?? -1;
+
+  HttpException([this._code, this._message]);
 
   @override
   String toString() {
-    return "code: $code message: $message";
+    return "code:$code--message=$message";
   }
 
-  factory AppException.create(DioError error) {
+  factory HttpException.create(DioError error) {
     switch (error.type) {
       case DioErrorType.cancel:
         {
           return BadRequestException(-1, "请求取消");
         }
-        break;
       case DioErrorType.connectionTimeout:
         {
           return BadRequestException(-1, "连接超时");
         }
-        break;
       case DioErrorType.sendTimeout:
         {
           return BadRequestException(-1, "请求超时");
         }
-        break;
       case DioErrorType.receiveTimeout:
         {
           return BadRequestException(-1, "响应超时");
         }
-        break;
       case DioErrorType.badResponse:
         {
           try {
@@ -46,72 +46,72 @@ class AppException implements Exception {
                 {
                   return BadRequestException(errCode, "请求语法错误");
                 }
-                break;
               case 401:
                 {
                   return UnauthorisedException(errCode, "没有权限");
                 }
-                break;
               case 403:
                 {
                   return UnauthorisedException(errCode, "服务器拒绝执行");
                 }
-                break;
               case 404:
                 {
                   return UnauthorisedException(errCode, "无法连接服务器");
                 }
-                break;
               case 405:
                 {
                   return UnauthorisedException(errCode, "请求方法被禁止");
                 }
-                break;
               case 500:
                 {
                   return UnauthorisedException(errCode, "服务器内部错误");
                 }
-                break;
               case 502:
                 {
                   return UnauthorisedException(errCode, "无效的请求");
                 }
-                break;
               case 503:
                 {
                   return UnauthorisedException(errCode, "服务器挂了");
                 }
-                break;
               case 505:
                 {
                   return UnauthorisedException(errCode, "不支持HTTP协议请求");
                 }
-                break;
               default:
                 {
                   // return ErrorEntity(code: errCode, message: "未知错误");
-                  return AppException(code: errCode, message: error.response?.statusMessage ?? '未知错误');
+                  return HttpException(
+                      errCode, error.response?.statusMessage ?? '未知错误');
                 }
             }
           } on Exception catch (_) {
-            return AppException(code: -1, message: "未知错误");
+            return HttpException(-1, "未知错误");
           }
         }
-        break;
       default:
         {
-          return AppException(code: -1, message: error.message ?? "未知错误");
+          return HttpException(-1, error.message ?? "未知错误");
         }
     }
   }
 }
 
 /// 请求错误
-class BadRequestException extends AppException {
-  BadRequestException(int code, String message) : super(code: code, message: message);
+class BadRequestException extends HttpException {
+  BadRequestException(int code, String message) : super(code, message);
+}
+
+class BadResponseException extends HttpException {
+  dynamic data;
+  BadResponseException([this.data]) : super();
+}
+
+class UnknownException extends HttpException {
+  UnknownException([String? message]) : super(-1, message);
 }
 
 /// 未认证异常
-class UnauthorisedException extends AppException {
-  UnauthorisedException(int code, String message) : super(code: code, message: message);
+class UnauthorisedException extends HttpException {
+  UnauthorisedException(int code, String message) : super(code, message);
 }
